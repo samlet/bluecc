7fac94144f13de209a3a83d620a423be08bc4933
Samlet Wu <xiaofei.wu@gmail.com>
2021-03-09 16:05:03 +0800

    remove unused module

--- entity-meta/src/tests/mod.rs
+++
@@ -1,6 +0,0 @@
-#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
-
-#[cfg(test)]
-mod storage_tests;
-mod resource_tests;
-mod proto_tests;
--- entity-meta/src/tests/resource_tests.rs
+++
@@ -1,7 +0,0 @@
-/// ref: https://doc.rust-lang.org/std/macro.include_bytes.html
-#[test]
-fn includes_works() {
-    let bytes = include_bytes!("spanish.in");
-    assert_eq!(bytes, b"adi\xc3\xb3s\n");
-    print!("{}", String::from_utf8_lossy(bytes));
-}

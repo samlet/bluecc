17688af292cf710020661c2e87f4a47872bc3b19
Samlet Wu <xiaofei.wu@gmail.com>
2021-04-23 14:01:17 +0800

    add chrono and decimal support

--- contracts/cart/cc_example/lib.rs
+++ contracts/cart/cc_example/lib.rs
@@ -1,9 +1,47 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_mut))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use ink_lang as ink;
 
 #[ink::contract]
 mod cc_example {
+    use ink_storage::{
+        collections::{HashMap as StorageHashMap, Stash as StorageStash, Vec as StorageVec},
+        traits::{PackedLayout, SpreadLayout}
+    };
+    use decimal::prelude::ToPrimitive;
+    use chrono::prelude::*;
+    use ink_prelude::vec::Vec;
+    use ink_prelude::string::String;
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout, Default)]
+    #[cfg_attr(feature = "std",
+        derive(Debug,PartialEq,Eq,
+        scale_info::TypeInfo,
+        ink_storage::traits::StorageLayout
+        )
+    )]
+    pub struct Decimal{
+        num: i64, scale: u32,
+    }
+
+    impl Decimal{
+        pub fn new(num: i64, scale: u32) -> Self {
+            Decimal { num, scale }
+        }
+    }
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(feature = "std",
+        derive(Debug,PartialEq,Eq,
+        scale_info::TypeInfo,
+        ink_storage::traits::StorageLayout
+        )
+    )]
+    pub struct ItemSpec {
+        // id: Vec<u8>,  // id
+        id: String,
+    }
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -12,13 +50,20 @@ mod cc_example {
     pub struct CcExample {
         /// Stores a single `bool` value on the storage.
         value: bool,
+        f_val: Decimal,
+        str_val: String,
+        item_spec: StorageStash<ItemSpec>,
     }
 
     impl CcExample {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+            Self { value: init_value,
+                f_val: Default::default(),
+                str_val: String::from("2014-11-28 12:00:09"),
+                item_spec: Default::default(),
+            }
         }
 
         /// Constructor that initializes the `bool` value to `false`.
@@ -35,12 +80,27 @@ mod cc_example {
         #[ink(message)]
         pub fn flip(&mut self) {
             self.value = !self.value;
+            self.f_val= Decimal::new(1302,2);
         }
 
         /// Simply returns the current value of our `bool`.
         #[ink(message)]
         pub fn get(&self) -> bool {
             self.value
+        }
+
+        #[ink(message)]
+        pub fn get_fval(&self) -> i64 {
+        // pub fn get_fval(&self) -> f64 {
+            let val=decimal::Decimal::new(self.f_val.num, self.f_val.scale);
+            val.to_i64().unwrap()
+        }
+
+        #[ink(message)]
+        pub fn get_dt_rfc3339(&self) -> String{
+            let dt=Utc.datetime_from_str(self.str_val.as_str(), "%Y-%m-%d %H:%M:%S").unwrap();
+            // dt.to_rfc3339().into_bytes()
+            dt.to_rfc3339()
         }
     }
 
@@ -69,6 +129,21 @@ mod cc_example {
             assert_eq!(cc_example.get(), false);
             cc_example.flip();
             assert_eq!(cc_example.get(), true);
+        }
+
+        #[ink::test]
+        fn decimal_works() {
+            let mut cc_example = CcExample::new(false);
+            cc_example.flip();
+            let fval=cc_example.get_fval();
+            println!("{}", fval);
+        }
+
+        #[ink::test]
+        fn dt_val_works() {
+            let mut cc_example = CcExample::new(false);
+            let rfc=cc_example.get_dt_rfc3339();
+            println!("{:?}", rfc);
         }
     }
 }
